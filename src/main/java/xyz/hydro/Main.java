21f3183acbf25dbdff9163b0deb93d8e9150d8a9
jiggey1 <fortniitetradezback@gmail.com>
2021-10-07 21:06:39 +0100

    Added locations and messages config

--- src/main/java/xyz/hydro/Main.java
+++ src/main/java/xyz/hydro/Main.java
@@ -1,6 +1,9 @@
 package xyz.hydro;
 
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -10,6 +13,10 @@ import xyz.hydro.events.OnRespawnEvent;
 import xyz.hydro.features.BoostPads;
 import xyz.hydro.gui.BoostPadsGUI;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Objects;
 
 public final class Main extends JavaPlugin {
@@ -19,6 +26,12 @@ public final class Main extends JavaPlugin {
     public static double pluginVersion;
 
     public static String pluginPrefix;
+
+    private File messagesConfigFile;
+    private FileConfiguration messagesConfig;
+
+    private File locationsConfigFile;
+    private FileConfiguration locationsConfig;
 
     public static String pluginContributors;
 
@@ -75,11 +88,110 @@ public final class Main extends JavaPlugin {
         getConfig().options().copyDefaults();
         saveDefaultConfig();
 
+        this.createMessagesConfig();
+        this.reloadMessagesConfig();
+        this.saveMessagesConfig();
+
+        this.createLocationsConfig();
+        this.reloadLocationsConfig();
+        this.saveLocationConfig();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+
+    // Messages.yml File Creation.
+    public FileConfiguration getMessagesConfig() {
+        return this.messagesConfig;
+    }
+
+    public void saveMessagesConfig() {
+        if(this.messagesConfig == null) {
+            return;
+        }
+
+        try {
+            this.getMessagesConfig().save(this.messagesConfigFile);
+        } catch(IOException e) {
+            // Being naughty and not logging :o
+        }
+    }
+
+    public void reloadMessagesConfig() {
+        if (this.messagesConfig == null) {
+            createMessagesConfig();
+        }
+        this.messagesConfig = YamlConfiguration.loadConfiguration(this.messagesConfigFile);
+
+        InputStream defaultStream = this.getResource("messages.yml");
+        if (defaultStream != null) {
+            YamlConfiguration defaultConfig = YamlConfiguration.loadConfiguration(new InputStreamReader(defaultStream));
+            this.messagesConfig.setDefaults(defaultConfig);
+        }
+    }
+
+    private void createMessagesConfig() {
+        messagesConfigFile = new File(getDataFolder(), "messages.yml");
+        if (!messagesConfigFile.exists()) {
+            messagesConfigFile.getParentFile().mkdirs();
+            saveResource("messages.yml", true);
+        }
+
+        messagesConfig = new YamlConfiguration();
+        try {
+            messagesConfig.load(messagesConfigFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    // Locations.yml File Creation.
+    public FileConfiguration getLocationsConfig() {
+        return this.locationsConfig;
+    }
+
+    public void saveLocationConfig() {
+        if(this.locationsConfig == null) {
+            return;
+        }
+
+        try {
+            this.getLocationsConfig().save(this.locationsConfigFile);
+        } catch(IOException e) {
+            // Being naughty and not logging :o
+        }
+    }
+
+    public void reloadLocationsConfig() {
+        if (this.locationsConfig == null) {
+            createLocationsConfig();
+        }
+        this.locationsConfig = YamlConfiguration.loadConfiguration(this.locationsConfigFile);
+
+        InputStream defaultStream = this.getResource("locations.yml");
+        if (defaultStream != null) {
+            YamlConfiguration defaultConfig = YamlConfiguration.loadConfiguration(new InputStreamReader(defaultStream));
+            this.locationsConfig.setDefaults(defaultConfig);
+        }
+    }
+
+    private void createLocationsConfig() {
+        locationsConfigFile = new File(getDataFolder(), "locations.yml");
+        if (!locationsConfigFile.exists()) {
+            locationsConfigFile.getParentFile().mkdirs();
+            saveResource("locations.yml", true);
+        }
+
+        locationsConfig = new YamlConfiguration();
+        try {
+            locationsConfig.load(locationsConfigFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
     }
 
 
