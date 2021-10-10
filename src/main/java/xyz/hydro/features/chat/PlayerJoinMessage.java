2bf4f7cc22cd09df473f5b3fbe56641a701faada
jiggey1 <fortniitetradezback@gmail.com>
2021-10-10 17:00:53 +0100

    Optimised, Improved, Added PAPI support.

--- src/main/java/xyz/hydro/features/chat/PlayerJoinMessage.java
+++ src/main/java/xyz/hydro/features/chat/PlayerJoinMessage.java
@@ -1,5 +1,6 @@
 package xyz.hydro.features.chat;
 
+import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
@@ -23,6 +24,30 @@ public class PlayerJoinMessage implements Listener {
 
         if(plugin.getConfig().getBoolean("announcePlayerJoin")) {
 
+            String joinDefault = format(plugin.getConfig().getString("defaultJoinMessage"));
+            joinDefault = PlaceholderAPI.setPlaceholders(player, joinDefault);
+
+            String joinRank1 = format(plugin.getConfig().getString("rankOneJoinMessage"));
+            joinRank1 = PlaceholderAPI.setPlaceholders(player, joinRank1);
+
+            String joinRank2 = format(plugin.getConfig().getString("rankTwoJoinMessage"));
+            joinRank2 = PlaceholderAPI.setPlaceholders(player, joinRank2);
+
+            String joinRank3 = format(plugin.getConfig().getString("rankThreeJoinMessage"));
+            joinRank3 = PlaceholderAPI.setPlaceholders(player, joinRank3);
+
+            String joinRank4 = format(plugin.getConfig().getString("rankFourJoinMessage"));
+            joinRank4 = PlaceholderAPI.setPlaceholders(player, joinRank4);
+
+            String joinRank5 = format(plugin.getConfig().getString("rankFiveJoinMessage"));
+            joinRank5 = PlaceholderAPI.setPlaceholders(player, joinRank5);
+
+            String joinAdmin = format(plugin.getConfig().getString("adminJoinMessage"));
+            joinAdmin = PlaceholderAPI.setPlaceholders(player, joinAdmin);
+
+            String joinOp = format(plugin.getConfig().getString("opJoinMessage"));
+            joinOp = PlaceholderAPI.setPlaceholders(player, joinOp);
+
             // Member Messages
             String defaultJoin = "hhub.joinMessages.defaultMessagePermission";
             String rank1Join = "hhub.joinMessages.rankOneMessagePermission";
@@ -36,54 +61,56 @@ public class PlayerJoinMessage implements Listener {
             String opJoin = "hhub.joinMessage.opMessagePermission";
 
 
+            if(player.isOp()) {
+                Bukkit.broadcastMessage(joinOp);
+                return;
+            }
+
             if(player.hasPermission(defaultJoin) && !player.hasPermission(rank1Join) || !player.hasPermission(rank2Join) || !player.hasPermission(rank3Join) || !player.hasPermission(rank4Join) ||
                     !player.hasPermission(rank5Join) && !player.hasPermission(adminJoin) && !player.hasPermission(opJoin)) {
-                Bukkit.broadcastMessage(Objects.requireNonNull(plugin.getMessagesConfig().getString("defaultJoinMessage")));
+                Bukkit.broadcastMessage(joinDefault);
                 return;
             }
 
             if(!player.hasPermission(defaultJoin) && player.hasPermission(rank1Join) || !player.hasPermission(rank2Join) || !player.hasPermission(rank3Join) || !player.hasPermission(rank4Join) ||
                     !player.hasPermission(rank5Join) && !player.hasPermission(adminJoin) && !player.hasPermission(opJoin)) {
-                Bukkit.broadcastMessage(Objects.requireNonNull(plugin.getMessagesConfig().getString("rankOneJoinMessage")));
+                Bukkit.broadcastMessage(joinRank1);
                 return;
             }
 
             if(!player.hasPermission(defaultJoin) && !player.hasPermission(rank1Join) || player.hasPermission(rank2Join) || !player.hasPermission(rank3Join) || !player.hasPermission(rank4Join) ||
                     !player.hasPermission(rank5Join) && !player.hasPermission(adminJoin) && !player.hasPermission(opJoin)) {
-                Bukkit.broadcastMessage(Objects.requireNonNull(plugin.getMessagesConfig().getString("rankTwoJoinMessage")));
+                Bukkit.broadcastMessage(joinRank2);
                 return;
             }
 
             if(!player.hasPermission(defaultJoin) && !player.hasPermission(rank1Join) || !player.hasPermission(rank2Join) || player.hasPermission(rank3Join) || !player.hasPermission(rank4Join) ||
                     !player.hasPermission(rank5Join) && !player.hasPermission(adminJoin) && !player.hasPermission(opJoin)) {
-                Bukkit.broadcastMessage(Objects.requireNonNull(plugin.getMessagesConfig().getString("rankThreeJoinMessage")));
+                Bukkit.broadcastMessage(joinRank3);
                 return;
             }
 
             if(!player.hasPermission(defaultJoin) && !player.hasPermission(rank1Join) || !player.hasPermission(rank2Join) || !player.hasPermission(rank3Join) || player.hasPermission(rank4Join) ||
                     !player.hasPermission(rank5Join) && !player.hasPermission(adminJoin) && !player.hasPermission(opJoin)) {
-                Bukkit.broadcastMessage(Objects.requireNonNull(plugin.getMessagesConfig().getString("rankFourJoinMessage")));
+                Bukkit.broadcastMessage(joinRank4);
                 return;
             }
 
             if(!player.hasPermission(defaultJoin) && !player.hasPermission(rank1Join) || !player.hasPermission(rank2Join) || !player.hasPermission(rank3Join) || !player.hasPermission(rank4Join) ||
                     player.hasPermission(rank5Join) && !player.hasPermission(adminJoin) && !player.hasPermission(opJoin)) {
-                Bukkit.broadcastMessage(Objects.requireNonNull(plugin.getMessagesConfig().getString("rankFiveJoinMessage")));
+                Bukkit.broadcastMessage(joinRank5);
                 return;
             }
 
             if(!player.hasPermission(defaultJoin) && !player.hasPermission(rank1Join) || !player.hasPermission(rank2Join) || !player.hasPermission(rank3Join) || !player.hasPermission(rank4Join) ||
                     !player.hasPermission(rank5Join) && player.hasPermission(adminJoin) && !player.hasPermission(opJoin)) {
-                Bukkit.broadcastMessage(Objects.requireNonNull(plugin.getMessagesConfig().getString("adminJoinMessage")));
+                Bukkit.broadcastMessage(joinAdmin);
                 return;
             }
 
-            if(!player.hasPermission(defaultJoin) && !player.hasPermission(rank1Join) || !player.hasPermission(rank2Join) || !player.hasPermission(rank3Join) || !player.hasPermission(rank4Join) ||
-                    !player.hasPermission(rank5Join) && !player.hasPermission(adminJoin) && player.hasPermission(opJoin)) {
-                Bukkit.broadcastMessage(Objects.requireNonNull(plugin.getMessagesConfig().getString("opJoinMessage")));
-            }
-
         }
+
+        // Does nothing since it's disabled in config.
 
     }
 
